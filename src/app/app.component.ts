c09b8fab3d1584194be5f7ee91202c6b32e3afa2
Matheus_Gomes <mathgomes007@htomail.com>
2023-04-11 21:34:10 -0300

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'corretor_de_imoveis_front_end';
+}
